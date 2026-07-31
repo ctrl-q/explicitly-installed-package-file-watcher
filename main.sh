85516718b2b0427d2c2ac7c9a08234ad7520f515
ctrl-q <34975747+ctrl-q@users.noreply.github.com>
2026-07-31 09:36:36 -0400

    feat: Add support for pi

--- main.sh
+++ main.sh
@@ -12,6 +12,7 @@ readonly obsidian_plugins_file="${EXPLICIT_PACKAGE_FILE_WATCHER_OBSIDIAN_PLUGINS
 readonly uv_tools_dir="${EXPLICIT_PACKAGE_FILE_WATCHER_UV_TOOLS_DIR:-${XDG_DATA_HOME}/uv/tools}"
 readonly vim_systemwide_plugin_dir="${EXPLICIT_PACKAGE_FILE_WATCHER_VIM_SYSTEMWIDE_PLUGIN_DIR:-/usr/share/vim/vimfiles/plugin}"
 readonly vimplug_dir="${EXPLICIT_PACKAGE_FILE_WATCHER_VIMPLUG_DIR:-${HOME}/.vim/plugged}"
+readonly pi_config_file="${EXPLICIT_PACKAGE_FILE_WATCHER_PI_CONFIG_FILE:-${HOME}/.pi/agent/settings.json}"
 
 get_explicitly_installed_packages(){
     pikaur -Qqe
@@ -21,6 +22,7 @@ get_explicitly_installed_packages(){
 	find "${vimplug_dir}" "${vim_systemwide_plugin_dir}" -mindepth 1 -maxdepth 1 -printf 'vim:%f\n'
 	find "${eget_packages_dir}" -mindepth 1 -printf '%P\n'
 	jq -r -c '.dependencies | keys | map("npm:" + .)[]' "${bun_package_file}"
+	jq -r -c '.packages[]' "${pi_config_file}"
 	ya pkg list | awk '/\(/ {print "yazi:" $1}'
 	find "${uv_tools_dir}" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | awk -F'- ' '/^- / {print "uv:" $2}'
 }
